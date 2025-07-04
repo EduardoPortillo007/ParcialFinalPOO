1e4f14d4433943e4f0beb44c31875231c772d535
EduardoPortillo007 <00044923@uca.edu.sv>
2025-07-04 12:57:37 -0600

    :memo:Clase terminada

--- ServidorCuadrado/src/servidorcuadrado/ServidorCuadrado.java
+++ ServidorCuadrado/src/servidorcuadrado/ServidorCuadrado.java
@@ -7,38 +7,21 @@ package servidorcuadrado;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.InputStreamReader;
 
 public class ServidorCuadrado {
 
     
     public static void main(String[] args) {
-        try{          
-            ServerSocket serverSo = new ServerSocket(5000);
-           
-            while(true){
-            System.out.println("Esperando la conexión");
-            Socket socketCl = serverSo.accept();
-            System.out.println("Cliente conectado: "+ socketCl.getInetAddress());
-           
-            InputStream inputStream = socketCl.getInputStream();
-            OutputStream outputStream = socketCl.getOutputStream();
-           
-            BufferedReader read = new BufferedReader(new InputStreamReader(inputStream));
-            PrintWriter write = new PrintWriter(outputStream, true);
-           
-            String msg = read.readLine();
-            System.out.println("Mensaje cliente: "+msg);
-            write.println("Este mensaje se ha enviado desde el servidor");
-            socketCl.close();
-            serverSo.close();
+        int puerto = 5000;
+        try (ServerSocket servidor = new ServerSocket(puerto)) {
+            System.out.println("Servidor iniciado en el puerto " + puerto);
+            while (true) {
+                Socket socketCliente = servidor.accept();
+                Thread hilo = new Thread(new ManejadorCliente(socketCliente));
+                hilo.start();
             }
-        }catch(IOException e){
-           
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
